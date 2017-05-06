2e204719f88a0cba80a132207ea36d017fefd1b5
alex2c8 <alex.dinu07@gmail.com>
2017-05-07 01:48:51 +0300

    cleaning

--- C++/utils/execution time.cpp
+++
@@ -1,9 +0,0 @@
-#include <time.h>
-int main(void) {
-    clock_t tStart = clock();
-
-    /* Do your stuff here */
-
-    printf("Time taken: %.4fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
-    return 0;
-}
