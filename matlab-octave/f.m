6fe26c3fc5eb202a6b752bb84ec61c09f55b733b
alex2c8 <alex.dinu07@gmail.com>
2017-08-24 14:13:27 +0300

    cleaning

--- matlab-octave/f.m
+++
@@ -1,9 +0,0 @@
-function [ v ] = f( n )
-%F Summary of this function goes here
-%   Detailed explanation goes here
-
-
-
-
-end
-
--- matlab-octave/vavg.m
+++
@@ -1,7 +0,0 @@
-function [ s ] = vavg(v)
-	s = 0;
-	for x = v
-		s = s + x;
-	end
-	printf('v_avg = %f\n', s / length(v));
-end
