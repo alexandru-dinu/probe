7a7505a600c1e8946544c7d75fde514557bb9ce8
alex2c8 <alex.dinu07@gmail.com>
2017-08-28 14:25:04 +0300

    move semantics, attempt 1

--- c++/functional/functional.hpp
+++ c++/functional/functional.hpp
@@ -6,131 +6,68 @@
 #include <algorithm>
 #include <vector>
 
-namespace func {
-
-	template <typename Collection>
-	bool empty(Collection c)
+namespace func
+{
+	template <typename T>
+	bool empty(std::vector<T> &c)
 	{
 		return c.size() == 0;
 	}
 
-	template <typename Collection>
-	unsigned int length(Collection c)
+	template <typename T>
+	uint length(std::vector<T> &c)
 	{
 		return c.size();
 	}
 
-	template <typename Collection>
-	typename Collection::value_type head(Collection c)
+	template <typename T>
+	T head(std::vector<T> &c)
 	{
 		return c.at(0);
 	}
 
-	template <typename ArgType, typename Pred>
-	std::function<bool(ArgType)> negate_pred(Pred &pred)
+	template <typename Op, typename T>
+	void foreach(Op op, std::vector<T> &c)
 	{
-		return [&pred](ArgType t) { return !pred(t); };
+		std::for_each(c.begin(), c.end(), op);
 	}
 
-
-
-	template <typename Elem, typename Collection>
-	Collection cons(Elem x, Collection c)
+	template <typename T>
+	std::vector<T>& cons_ms(T x, std::vector<T> &&c)
 	{
 		c.insert(c.begin(), x);
 		return c;
 	}
 
-	template <typename Elem, typename Collection>
-	void cons_r(Elem x, Collection &c)
-	{
-		c.insert (c.begin(), x);
-	}
-
-
-
-	template <typename Collection>
-	Collection take(unsigned int p, Collection c)
-	{
-		p = (p >= c.size()) ? c.size() : p;
-		c.erase(c.begin() + p, c.end());
-		return c;
-	}
-
-	template <typename Collection>
-	void take_r(unsigned int p, Collection &c)
-	{
-		p = (p >= c.size()) ? c.size() : p;
-		c.erase(c.begin() + p, c.end());
-	}
-
-
-
-	template <typename Collection>
-	Collection drop(unsigned int p, Collection c)
-	{
-		p = (p >= c.size()) ? c.size() : p;
-		c.erase(c.begin(), c.begin() + p);
-		return c;
-	}
-
-	template <typename Collection>
-	void drop_r(unsigned int p, Collection &c)
-	{
-		p = (p >= c.size()) ? c.size() : p;
-		c.erase(c.begin(), c.begin() + p);
-	}
-
-
-
-	template <typename Collection>
-	Collection tail(Collection c)
+	template <typename T>
+	std::vector<T>& tail_ms(std::vector<T> &c)
 	{
 		c.erase(c.begin(), c.begin() + 1);
 		return c;
 	}
 
-	template <typename Collection>
-	void tail_r(Collection &c)
+	template <typename T>
+	std::vector<T> take(uint n, std::vector<T> &c)
 	{
-		c.erase(c.begin(), c.begin() + 1);
+		n = (n >= c.size()) ? c.size() : n;
+		std::vector<T> r(c.begin(), c.begin() + n);
+		return r;
 	}
 
-
-
-	template <typename UnaryOperator, typename Collection>
-	void foreach(UnaryOperator op, Collection c)
+	template <typename T>
+	std::vector<T> drop(uint n, std::vector<T> &c)
 	{
-		std::for_each(c.begin(), c.end(), op);
+		n = (n >= c.size()) ? c.size() : n;
+		std::vector<T> r(c.begin() + n, c.end());
+		return r;
 	}
 
-
-	// higher-order functions
-
-	// template <typename UnaryOperator, typename Collection>
-	// void map(UnaryOperator op, Collection &c)
+	// template <typename ArgType, typename Pred>
+	// std::function<bool(ArgType)> negate_pred(Pred &pred)
 	// {
-	// 	std::transform(c.begin(), c.end(), c.begin(), op);
+	// 	return [&pred](ArgType t) { return !pred(t); };
 	// }
 
-	// template <typename BinaryOperator, typename Collection>
-	// Collection zipWith(BinaryOperator op, Collection c1, Collection c2)
-	// {
-	// 	Collection r = c1;
-	// 	std::transform(r.begin(), r.end(), c2.begin(), r.begin(), op);
-	// 	return r;
-	// }
-
-	// template <typename Predicate, typename Collection>
-	// Collection filter(Predicate pred, Collection c)
-	// {
-	// 	Collection r = c;
-	// 	r.erase(std::remove_if(
-	// 			r.begin(), r.end(),
-	// 			[pred](typename Collection::value_type i) { return !pred(i);}),
-	// 			std::end(r));
-	// 	return r;
-	// }
 
 } // namespace func
 
--- c++/functional/mergesort.cpp
+++ c++/functional/mergesort.cpp
@@ -9,8 +9,6 @@
 
 using namespace func;
 
-typedef std::vector<int> list;
-
 #define N 50000
 
 auto echo = [](int x) {std::cout << x << std::endl;};
@@ -19,31 +17,58 @@ auto sub = [](int x, int y) {return x - y;};
 auto even = [](int x) {return (x % 2 == 0);};
 
 
+template <typename T>
+std::vector<T> merge(std::vector<T> v1, std::vector<T> v2)
+{
+    if (empty(v1))
+        return v2;
+    if (empty(v2))
+        return v1;
+
+    T x1 = head(v1);
+    T x2 = head(v2);
+
+    if (x1 < x2)
+        return cons_ms(x1, std::move(merge(tail_ms(v1), v2)));
+    else
+        return cons_ms(x2, std::move(merge(v1, tail_ms(v2))));
+}
+
+template <typename T>
+std::vector<T> mergesort(std::vector<T> v)
+{
+    if (length(v) <= 1)
+        return v;
+
+    int mid = length(v) / 2;
+
+    std::vector<T> l = take(mid, v);
+    std::vector<T> r = drop(mid, v);
+
+    return merge(mergesort(std::move(l)), mergesort(std::move(r)));
+}
 
 
-int main() {
-
-    list v1 = {3, 2, 1};
-
-
-    // test with large vector
-    list v2(N);
-
-    std::random_device rd;
-    std::default_random_engine dre(rd());
-    std::uniform_int_distribution<int> uid(0,1000);
+int main()
+{
+    std::vector<int> v(N);
 
     for (int i = 0; i < N; i++)
-        v2[i] = uid(dre);
+        v[i] = N - i;
 
     auto begin = std::chrono::high_resolution_clock::now();
 
-    // TODO
+    std::vector<int> s = mergesort(v);
 
     auto end = std::chrono::high_resolution_clock::now();
-    std::cout <<
-        std::chrono::duration_cast<std::chrono::nanoseconds>(end-begin).count() * 1e-6 <<
-        "ms" << std::endl;
+    std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(end-begin).count() * 1e-6 << "ms" << std::endl;
+
+    for (int i = 0; i < N - 1; i++) {
+        if (s[i + 1] < s[i])
+            return 1;
+    }
+
+    std::cout << "OK\n";
 
     return 0;
 }
