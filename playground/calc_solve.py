ebb95ef9d4deabc0162d147bd73cca1d70982f70
alexandru-dinu <alex.dinu07@gmail.com>
2017-10-16 01:59:41 +0300

    solve calculator: the game

--- playground/calc_solve.py
+++ playground/calc_solve.py
@@ -6,6 +6,8 @@ https://play.google.com/store/apps/details?id=com.sm.calculateme&hl=en
 sign = lambda x: (1, -1)[x < 0]
 strip_sign = lambda x : x[1:] if x[0] == '-' else x
 
+ss = lambda x : strip_sign(str(x))
+
 expand = lambda x : [(name, f(x)) for (name, f) in ops.iteritems()]
 
 VALUE = 0
@@ -64,35 +66,27 @@ def solve(tree, target):
 """
 Game parameters
 """
-initial = 356
-target = 121
+initial = 9
+target = 30
 moves = 4
 ops = {
-    # "*2" : lambda x : x  * 2,
-    # "10" : lambda x : int(str(x) + ('10')),
-    # "^3" : lambda x : int(x ** 3),
-    # "10->1" : lambda x : int(str(x).replace('10', '1')),
-    #"SUM" : lambda x : sign(x) * sum([int(c) for c in strip_sign(str(x))]),
+    # "*2" : lambda x : x  * 2
+    # "^3" : lambda x : int(x ** 3)
 
-    # "-9" : lambda x : x - 9,
-    # "*3" : lambda x : x * 3,
-    # "+4" : lambda x : x + 4,
-    # "+/-" : lambda x : (-1) * x,
-    # "Rev" : lambda x : sign(x) * int(strip_sign(str(x))[::-1])
+    # "0" : lambda x : int(str(x) + ('0'))
+    # "10->1" : lambda x : int(str(x).replace('10', '1'))
 
-    # "<<" : lambda x : 0 if len(strip_sign(str(x))) == 1 else int(str(x)[:-1]),
-    # "+6" : lambda x : x + 6,
-    # "-3" : lambda x : x - 3,
+    # "SUM" : lambda x : sign(x) * sum([int(c) for c in ss(x))]),
+    # "Rev" : lambda x : sign(x) * int(ss(x)[::-1])
 
-    # "1" : lambda x : int(str(x) + ('1')),
-    # "*3" : lambda x : x * 3,
-    # "Rev" : lambda x : sign(x) * int(strip_sign(str(x))[::-1])
+    # "<<" : lambda x : 0 if len(ss(x))) == 1 else int(str(x)[:-1])
+    # "S<" : lambda x : sign(x) * int(ss(x)[1:] + ss(x)[0]) 
+    # "S>" : lambda x : int(ss(x)[len(ss(x)))-1] + ss(x)[0:len(ss(x))-1])   
 
-    "-2" : lambda x : x - 2,
-    "/3" : lambda x : x / 3,
-    # "0" : lambda x : int(str(x) + ('0')),
-    #"S<" : lambda x : sign(x) * int(strip_sign(str(x))[1:] + strip_sign(str(x))[0]) 
-    "S>" : lambda x : int(strip_sign(str(x))[len(strip_sign(str(x)))-1] + strip_sign(str(x))[0:len(strip_sign(str(x)))-1])   
+    "-5" : lambda x : x - 5,
+    "*-6" : lambda x : x * (-6),
+    "+/-" : lambda x : x * (-1),
+    "SUM" : lambda x : sign(x) * sum([int(c) for c in ss(x)])
 }
 
 
@@ -100,6 +94,6 @@ if __name__ == '__main__':
 
     tree = cons_tree(initial, target, moves)
     #print_tree(tree)
-    
+
     for solution in solve(tree, target):
         print solution
