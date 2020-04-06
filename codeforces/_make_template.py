d472b6b033d5342d7131bc51ba3cf3a99f2b310e
Alexandru Dinu <alex.dinu07@gmail.com>
2020-04-07 01:45:29 +0800

    Add cpp template.

---
+++ codeforces/_make_template.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import glob
+
+assert len(sys.argv) > 1
+
+code = """
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+
+    return 0;
+}
+""".strip()
+
+last = int(sorted(glob.glob("*.cpp"))[-1].split('_')[0])
+name = '_'.join(map(lambda x: x.lower(), sys.argv[1:]))
+
+out_name = f'{last+1}_{name}.cpp'
+assert not os.path.exists(out_name)
+
+with open(out_name, 'wt') as fp:
+    fp.write(code)
