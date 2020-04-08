aa87a088d58f48db9488da9278728801e359185d
Alexandru Dinu <alex.dinu07@gmail.com>
2020-04-09 00:57:23 +0800

    Add codeforces round #632 (div. 2)

---
+++ codeforces/round_632_div_2/1_little_artem.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == n-1 and j == m-1)
+                    cout << "W";
+                else
+                    cout << "B";
+            }
+            cout << "\n";
+        }
+    }
+
+    return 0;
+}
