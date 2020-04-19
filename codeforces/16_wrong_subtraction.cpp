a13c74f81bc718a682d7c6976fe133010dc13d5c
Alexandru Dinu <alex.dinu07@gmail.com>
2020-04-20 02:41:53 +0800

    Codeforces.

---
+++ codeforces/16_wrong_subtraction.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+
+    int n, k;
+    cin >> n >> k;
+
+    while (k--) {
+        if (n % 10 == 0)
+            n /= 10;
+        else
+            n--;
+    }
+
+    cout << n;
+
+    return 0;
+}
