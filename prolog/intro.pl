724dda80ac4af7f78d1146ce153367281443ae62
Alexandru Dinu <alex.dinu07@gmail.com>
2020-04-28 06:43:57 +0800

    [prolog] add backtracking TO-DOs

--- prolog/intro.pl
+++
@@ -1,6 +0,0 @@
-isList([]).
-isList([_|_]).
-
-flatten([], []).
-flatten([H|T], [H|Rest]) :- not(isList(H)), flatten(T, Rest).
-flatten([H|T], Rest) :- isList(H), flatten(T, R1), append(H, R1, Rest).
