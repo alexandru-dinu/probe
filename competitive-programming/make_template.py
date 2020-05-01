613f0d025a5b695e40955e608a1d965be0846cb1
Alexandru Dinu <alex.dinu07@gmail.com>
2020-05-02 04:21:40 +0800

    [comp-prog] better template

--- competitive-programming/make_template.py
+++ competitive-programming/make_template.py
@@ -13,12 +13,26 @@ code = """
 using namespace std;
 
 #define ll long long
+#define sol_t void
+
+sol_t solve(int tc) {
+    ;
+
+    return;
+}
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(NULL);
 
-    ;
+    int tc;
+
+    //tc = 1;
+    cin >> tc;
+
+    while (tc--) {
+        solve(tc);
+    }
 
     return 0;
 }
