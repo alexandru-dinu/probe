1b733c26de7fabdc8084993c8e2514925f51dd9d
Alexandru Dinu <alex.dinu07@gmail.com>
2020-05-15 00:42:37 +0800

    [comp-prog] add codeforces round 640 div 4

---
+++ competitive-programming/codeforces/round_640_div_4/kth_not_divisible_by_n.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define sol_t void
+
+sol_t solve(int tc) {
+    ll n, k;
+    cin >> n >> k;
+
+    cout << k + (k-1)/(n-1) << "\n";
+
+    return;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+
+    int tc;
+
+    //tc = 1;
+    cin >> tc;
+
+    while (tc--) {
+        solve(tc);
+    }
+
+    return 0;
+}
